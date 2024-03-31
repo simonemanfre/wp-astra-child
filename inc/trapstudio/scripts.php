cbfbdf354a8c1e4b4108337b9f62f4d2ce854eb1
simonemanfre <simone.manfredini@trapella.it>
2024-03-31 20:32:12 +0200

    feat: SCSS

--- inc/trapstudio/scripts.php
+++ inc/trapstudio/scripts.php
@@ -4,7 +4,8 @@ function trp_site_scripts_and_css() {
     $dati_tema = wp_get_theme();
 
     //FILE CSS
-    wp_enqueue_style( 'astra-child-theme-css', THEME_URL . '/style.css', array('astra-theme-css'), $dati_tema->Version, 'all' );
+    wp_enqueue_style( 'astra-child-theme', THEME_URL . '/style.css', array('astra-theme-css'), $dati_tema->Version );
+    wp_enqueue_style( 'general', THEME_URL . "/assets/css/general.css", array('astra-theme-css'), $dati_tema->Version);
     
     //FILE SCRIPTS
     wp_enqueue_script( 'siteScripts', THEME_URL . "/assets/js/scripts.js", array('jquery'), $dati_tema->Version, true );
