1935537864233928da3c800d4ad280249d48866b
simonemanfre <simone.manfredini@trapella.it>
2024-05-26 16:24:52 +0200

    feat: JS

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,12 +1,25 @@
 // jQuery
 jQuery(document).ready(function ($) {
-    // Load Scripts deferred
-    setTimeout(function () {
-        // JS
-        const scripts = document.querySelectorAll("script[data-src]");
+  // Link Truendo
+  jQuery(".j-cookie-policy").click(function () {
+    Truendo.openCookieSettings();
 
-        scripts.forEach((script) => {
-            script.src = script.dataset.src;
-        });
-    }, 1500);
+    return false;
+  });
+
+  jQuery(".j-privacy-policy").click(function () {
+    Truendo.openYourRights();
+
+    return false;
+  });
+
+  // Load Scripts deferred
+  setTimeout(function () {
+    // JS
+    const scripts = document.querySelectorAll("script[data-src]");
+
+    scripts.forEach((script) => {
+      script.src = script.dataset.src;
+    });
+  }, 1500);
 });
